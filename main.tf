7609ddb4c00cf1e5cb63d4545aaa0ca765d3b960
sakeharicharan <Sakeharicharan0123@gmail.com>
2025-10-29 19:44:58 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_instance" "one" {
+  count                  = 4
+  ami                    = "ami-00af95fa354fdb788"
+  instance_type          = "t3.medium"
+  key_name               = "Netflix"
+  vpc_security_group_ids = ["sg-06f5dea461aa96704"]
+  tags = {
+    Name = var.instance_names[count.index]
+  }
+}
+
+variable "instance_names" {
+  default = ["jenkins", "tomcat-1", "tomcat-2", "Monitoring server"]
+}
